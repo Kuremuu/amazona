4e42f10e6e71f1a7c5d7f81cb4390f6148361049
unknown <clemvincentplumo@gmail.com>
2022-12-05 11:13:00 +0800

    initial commit

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,14 +1,14 @@
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import { Link } from 'react-router-dom';
-import Rating from './Rating';
-import axios from 'axios';
-import { useContext } from 'react';
-import { Store } from '../Store';
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
+import Rating from "./Rating";
+import axios from "axios";
+import { useContext, useState } from "react";
+import { Store } from "../Store";
 
 function Product(props) {
   const { product } = props;
-
+  const [image, setImage] = useState(product.image);
   const { state, dispatch: ctxDispatch } = useContext(Store);
   const {
     cart: { cartItems },
@@ -19,11 +19,11 @@ function Product(props) {
     const quantity = existItem ? existItem.quantity + 1 : 1;
     const { data } = await axios.get(`/api/products/${item._id}`);
     if (data.countInStock < quantity) {
-      window.alert('Sorry. Product is out of stock');
+      window.alert("Sorry. Product is out of stock");
       return;
     }
     ctxDispatch({
-      type: 'CART_ADD_ITEM',
+      type: "CART_ADD_ITEM",
       payload: { ...item, quantity },
     });
   };
@@ -31,14 +31,24 @@ function Product(props) {
   return (
     <Card>
       <Link to={`/product/${product.slug}`}>
-        <img src={product.image} className="card-img-top" alt={product.name} />
+        <img
+          src={image}
+          className="card-img-top"
+          alt={product.name}
+          onError={() =>
+            setImage(
+              "	https://www.freeiconspng.com/uploads/no-image-icon-15.png"
+            )
+          }
+          style={{ width: "214px", height: "261px" }}
+        />
       </Link>
       <Card.Body>
         <Link to={`/product/${product.slug}`}>
           <Card.Title>{product.name}</Card.Title>
         </Link>
         <Rating rating={product.rating} numReviews={product.numReviews} />
-        <Card.Text>${product.price}</Card.Text>
+        <Card.Text>₱{product.price}</Card.Text>
         {product.countInStock === 0 ? (
           <Button variant="light" disabled>
             Out of stock
